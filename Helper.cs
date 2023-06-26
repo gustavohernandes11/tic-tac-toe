3685f9192102db72f88fc82882a7478d74629c66
gustavohernandes11 <gustavo.hernandes.s11@gmail.com>
2023-06-26 13:25:09 -0300

    feat: first version finished

--- Helper.cs
+++ Helper.cs
@@ -1,10 +1,10 @@
 public class Helper
 {
-    public static char AskForCharNumber(string text)
+    public static char AskForCharNumber(string prompt)
     {
         while (true)
         {
-            Console.WriteLine(text);
+            Console.WriteLine(prompt);
             ConsoleKeyInfo input = Console.ReadKey();
             char inputChar = input.KeyChar;
 
@@ -12,7 +12,34 @@ public class Helper
             else Console.WriteLine("Only numbers are allowed");
 
             Console.WriteLine();
-
         }
+    }
+
+    public static string ItemOrWhiteSpace(string item) =>
+        String.IsNullOrEmpty(item) ? " " : item;
+
+
+    public static bool IsEqualAtIndexes<T>(T[] array, int[] indexes)
+    {
+        if (indexes.Max<int>() + 1 > array.Length || indexes.Min<int>() < 0)
+            throw new Exception("Invalid index in indexes array.");
+
+        if (indexes.Length == 0)
+            throw new Exception("Indefined indexes.");
+
+        if (array.Length == 0)
+            return true;
+
+        T firstArrayItem = array[indexes[0]];
+
+        foreach (var index in indexes)
+        {
+            if (array[index] is string && String.IsNullOrWhiteSpace(array[index]?.ToString()))
+                return false;
+
+            if (!firstArrayItem!.Equals(array[index]))
+                return false;
+        }
+        return true;
     }
 }
--- TicTacToe.cs
+++ TicTacToe.cs
@@ -1,106 +1,154 @@
-namespace tic_tac_toe
+namespace tic_tac_toe;
+
+public class TicTacToe
 {
-    public class TicTacToe
+    string[] BoardContent { get; set; }
+    bool ShouldGameContinueRunning { get; set; } = true;
+
+    TurnManager turnManager = new TurnManager();
+    string[] emptyStrings = new string[9] { "", "", "", "", "", "", "", "", "" };
+
+    public TicTacToe()
     {
-        string[] BoardContent { get; set; }
-        TurnManager turnManager = new TurnManager();
-        bool ShouldGameContinueRunning { get; set; } = true;
+        BoardContent = emptyStrings;
+        turnManager = new TurnManager();
+    }
 
-        public TicTacToe()
+    public void Init()
+    {
+        Clear();
+        DisplayGameIntro();
+
+        while (ShouldGameContinueRunning)
         {
-            BoardContent = new string[9];
-            turnManager = new TurnManager();
-        }
+            DisplayFormattedBoard(BoardContent);
+            turnManager.DisplayCurrentTurn();
 
-        public void Init()
-        {
-            DisplayGameIntro();
 
-            while (ShouldGameContinueRunning)
-            {
-                DisplayFormattedBoard(BoardContent);
-                turnManager.DisplayCurrentTurn();
-
-                char userOption = Helper.AskForCharNumber("Select a cell position: ");
-                bool shouldToggleTurn = IsValidPosition(userOption);
-
-                if (shouldToggleTurn) turnManager.ToggleTurn();
-
-                VerifyGame(BoardContent);
-                Clear();
-            }
-            DisplayResults(BoardContent);
-        }
-
-        void DisplayGameIntro()
-        {
-            Console.WriteLine("Welcome to Tic-Tac-Toe Game!");
-            Console.WriteLine("Use the keyboard numbers to select the cell position like this:");
-
-            string[] numberStrings1to9 = new string[9] { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
-            DisplayFormattedBoard(numberStrings1to9);
+            char userOption = Helper.AskForCharNumber("Select a cell position: ");
             Console.WriteLine();
-        }
-        void DisplayFormattedBoard(string[] items)
-        {
-            Console.WriteLine($" {ItemOrWhiteSpace(items[0])} | {ItemOrWhiteSpace(items[1])} | {ItemOrWhiteSpace(items[2])} ");
-            Console.WriteLine($"---+---+---");
-            Console.WriteLine($" {ItemOrWhiteSpace(items[3])} | {ItemOrWhiteSpace(items[4])} | {ItemOrWhiteSpace(items[5])} ");
-            Console.WriteLine($"---+---+---");
-            Console.WriteLine($" {ItemOrWhiteSpace(items[6])} | {ItemOrWhiteSpace(items[7])} | {ItemOrWhiteSpace(items[8])} ");
+
+            bool shouldToggleTurn = IsValidPosition(userOption);
+            VerifyGame(BoardContent);
+
+            if (shouldToggleTurn) turnManager.ToggleTurn();
 
         }
-        string ItemOrWhiteSpace(string item) =>
-            String.IsNullOrEmpty(item) ? " " : item;
 
-        bool IsValidPosition(char position)
-        {
-            int index = int.Parse(position.ToString()) - 1;
-            bool IsAlreadyOccuped = !String.IsNullOrEmpty(BoardContent[index]);
-
-            if (IsAlreadyOccuped)
-            {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("This cell is already ocupped. Try again.");
-                Console.ForegroundColor = ConsoleColor.White;
-                return false;
-            }
-            else
-            {
-                BoardContent[index] = turnManager.CurrentTurn.ToString();
-                return true;
-            }
-
-        }
-        void VerifyGame(string[] BoardContent) => throw new NotImplementedException();
-        void DisplayResults(string[] BoardContent) => throw new NotImplementedException();
-        void Clear() => Console.Clear();
     }
-    public class TurnManager
-    {
-        public Player CurrentTurn { get; private set; }
 
-        public TurnManager()
+    void DisplayGameIntro()
+    {
+        Console.WriteLine("Welcome to Tic-Tac-Toe Game!");
+        Console.WriteLine("Use the keyboard numbers to select the cell position like this:");
+
+        string[] numberStrings1to9 = new string[9] { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+        DisplayFormattedBoard(numberStrings1to9);
+        Console.WriteLine();
+    }
+
+    void DisplayFormattedBoard(string[] items)
+    {
+        string[] positions = new string[9];
+
+        for (int i = 0; i < 9; i++)
         {
-            CurrentTurn = Player.X;
+            positions[i] = Helper.ItemOrWhiteSpace(items[i]);
         }
 
-        public void ToggleTurn() =>
-            CurrentTurn = (CurrentTurn == Player.X) ? Player.O : Player.X;
+        Console.WriteLine($" {positions[0]} | {positions[1]} | {positions[2]} ");
+        Console.WriteLine("---+---+---");
+        Console.WriteLine($" {positions[3]} | {positions[4]} | {positions[5]} ");
+        Console.WriteLine("---+---+---");
+        Console.WriteLine($" {positions[6]} | {positions[7]} | {positions[8]} ");
+    }
 
-        public void DisplayCurrentTurn()
+
+    bool IsValidPosition(char position)
+    {
+        int index = int.Parse(position.ToString()) - 1;
+        bool IsAlreadyOccuped = !String.IsNullOrEmpty(BoardContent[index]);
+
+        if (index < 0)
         {
-            Console.ForegroundColor = ConsoleColor.Blue;
-            Console.WriteLine($"{CurrentTurn} Player's Turn");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("0 Não é uma opção.");
             Console.ForegroundColor = ConsoleColor.White;
+            return false;
+        }
+
+        if (IsAlreadyOccuped)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("This cell is already ocupped. Try again.");
+            Console.ForegroundColor = ConsoleColor.White;
+            return false;
+        }
+        else
+        {
+            BoardContent[index] = turnManager.CurrentTurn.ToString();
+            return true;
         }
 
     }
-    public enum Player
+    void VerifyGame(string[] BoardContent)
     {
-        X, O
+        if (
+        // row
+        Helper.IsEqualAtIndexes(BoardContent, new int[] { 0, 1, 2 }) ||
+        Helper.IsEqualAtIndexes(BoardContent, new int[] { 3, 4, 5 }) ||
+        Helper.IsEqualAtIndexes(BoardContent, new int[] { 6, 7, 8 }) ||
+        // column
+        Helper.IsEqualAtIndexes(BoardContent, new int[] { 0, 3, 6 }) ||
+        Helper.IsEqualAtIndexes(BoardContent, new int[] { 1, 4, 7 }) ||
+        Helper.IsEqualAtIndexes(BoardContent, new int[] { 2, 5, 8 }) ||
+        // diagonal
+        Helper.IsEqualAtIndexes(BoardContent, new int[] { 0, 4, 8 }) ||
+        Helper.IsEqualAtIndexes(BoardContent, new int[] { 6, 4, 2 })
+        )
+        {
+            Clear();
+            ShouldGameContinueRunning = false;
+            DisplayResults(BoardContent);
+            DisplayFormattedBoard(BoardContent);
+        }
     }
+
+    void DisplayResults(string[] BoardContent)
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine($"{turnManager.CurrentTurn} Venceu!");
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+    void Clear() => Console.Clear();
 }
+
+public class TurnManager
+{
+    public Player CurrentTurn { get; private set; }
+
+    public TurnManager()
+    {
+        CurrentTurn = Player.X;
+    }
+
+    public void ToggleTurn() =>
+        CurrentTurn = (CurrentTurn == Player.X) ? Player.O : Player.X;
+
+    public void DisplayCurrentTurn()
+    {
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.WriteLine($"{CurrentTurn} Player's Turn");
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+
+}
+
+public enum Player
+{
+    X, O
+}
+
 
 
 
